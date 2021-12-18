68a311cb8b94601a377261260299f876636d90c9
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2021-12-18 17:49:07 +0100

    app withj container support

---
+++ k8s-test/frontend/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using frontend.Models;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http;
+
+namespace frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new HomeViewModel();
+            model.PageCreatedAt = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+            model.HostName = Environment.MachineName;
+            var httpConnectionFeature = HttpContext.Features.Get<IHttpConnectionFeature>();
+            var localIpAddress = httpConnectionFeature?.LocalIpAddress;
+            model.HostIpAddress = localIpAddress.ToString() ;
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ k8s-test/frontend/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace frontend.Models
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+        }
+
+        public string HostIpAddress { get; set; }
+        public string HostName { get; set; }
+        public string PageCreatedAt { get; set; }
+    }
+}
